613790415b516bda064204aeb26356fc776e57e5
Arash Moradi <144523233+arashmoradi94@users.noreply.github.com>
2024-12-16 02:12:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import telebot
 from telebot import TeleBot
 import pymysql
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, Float, BigInteger
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
 from datetime import datetime
 import hashlib
 from telebot import types
