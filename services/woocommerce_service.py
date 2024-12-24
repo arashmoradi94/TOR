ffe23c1c4eda24dfc83e44502d022195438434c6
Arash Moradi <144523233+arashmoradi94@users.noreply.github.com>
2024-12-24 16:43:27 +0330

    Update woocommerce_service.py

--- services/woocommerce_service.py
+++ services/woocommerce_service.py
@@ -1,1 +1,20 @@
+from woocommerce import API
+import aiohttp
+from database.operations import get_user
 
+async def get_woo_product_price(telegram_id: int, product_name: str):
+    user = await get_user(telegram_id)
+    if not user or not user.woo_key or not user.woo_secret:
+        return None
+        
+    wcapi = API(
+        url="your-woocommerce-site.com",
+        consumer_key=user.woo_key,
+        consumer_secret=user.woo_secret,
+        version="wc/v3"
+    )
+    
+    products = wcapi.get("products", params={"search": product_name}).json()
+    if products:
+        return float(products[0]["price"])
+    return None
